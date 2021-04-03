2c7bfbe6bf4c6d168fb6ab907d78c4f067b64ffd
HemaVyas <Hema@hkls-mbp.lan>
2021-04-03 08:50:46 -0400

    updated paragraph2 to count sentances

--- Challenge/Paragraph2.py
+++ Challenge/Paragraph2.py
@@ -7,6 +7,7 @@ number_of_lines = 0
 number_of_words = 0
 number_of_characters = 0
 sentance_list =[] 
+number_of_sentances = 0
 
 for line in paragraph:
   line = line.strip("\n")
@@ -15,15 +16,24 @@ for line in paragraph:
   number_of_lines += 1
   number_of_words += len(words)
   number_of_characters += len(line)
+  number_of_sentances = line.count('.') # can't get it to count when on multiple lines.
+
+
 #full_stops = 0  
 #for stop in lines:
 #    full_stops = full_stops + len(stop.count('.'))
 #print ('total stops:    ', full_stops)
 
-re.split(r'[.!?]+', paragraph)
+#re.split(r'[.!?]+', paragraph)
 
 paragraph.close()
 
-print("lines:", number_of_lines, "words:", number_of_words, "characters:", number_of_characters)
-print(words)
 print(len(words))
+print("                                ")
+print(f"Paragraph Analysis")
+print("------------------------------------")
+print(f"Approximate Word Count: {number_of_words}")
+print(f"Approximate Sentance Count: {number_of_sentances}")
+print(f"Average Letter Count: {format(number_of_characters/number_of_words, '.2f')}") #  this is to drive number of decimil places
+print(f"Average Sentance Length: {number_of_words /number_of_sentances} ")
+print("                                ")
